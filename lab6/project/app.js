26e116b84fdeec54f85bc23771285fb5d514fba3
jurgen <jurgenbarbier@gmail.com>
2014-05-14 18:26:39 +0200

    update app.json + dependencies to setup mongodb

--- lab6/project/app.js
+++ lab6/project/app.js
@@ -10,9 +10,12 @@ var user = require('./routes/user');
 var http = require('http');
 var path = require('path');
 
+//db vars
+var mongo = require('mongodb');
+var monk = require('monk');
+var db = monk('mongodb://jurgb:1308JuBa@oceanic.mongohq.com:10059/imdwall');
 
-
-
+//faye server variabelen&
 var bayeux = new faye.NodeAdapter({
     	mount: '/faye',
     	timeout: 45
@@ -24,24 +27,29 @@ bayeux.attach(server);
 
 
 // all environments
+app.use(express.favicon());
+app.use(express.static(__dirname, 'public'));
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
-app.use(express.favicon());
 app.use(express.logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded());
 app.use(express.methodOverride());
 app.use(app.router);
-app.use(express.static(path.join(__dirname, 'public')));
+
 
 // development only
 if ('development' == app.get('env')) {
   app.use(express.errorHandler());
 }
 
+//pages GET
 app.get('/', routes.index);
 app.get('/scoreboard', routes.scoreboard);
 app.get('/users', user.list);
+
+//pages POST
+
 
 server.listen(3000);
 console.log("App launched on port 3000");
